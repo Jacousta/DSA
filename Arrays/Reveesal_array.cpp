a99f039cf4faf8d2731d6b0500fa1deeaf23bc7e
Jacousta <asharma22_be21@thapar.edu>
2024-06-20 13:43:57 +0530

    first commit

---
+++ Arrays/Reveesal_array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,4};
+    cout<<"array after reversal\n";
+    for(int i=3;i>=0;i--)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
