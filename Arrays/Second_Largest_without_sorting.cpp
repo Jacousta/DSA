7e8db8998cb8d52aaa2feac33bf11097a8d50732
Jacousta <asharma22_be21@thapar.edu>
2024-07-11 12:29:17 +0530

    Arraysuploaded

--- Arrays/Second_Largest_without_sorting.cpp
+++ Arrays/Second_Largest_without_sorting.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    
-}
+// classSolution {
+//   public:
+//     // Function returns the second
+//     // largest elements
+//     int largest=-1;
+//     int secondlargest=-1;
+//     int print2largest(vector<int> &arr) {
+//         // Code Here
+//         int n=arr.size();
+//         for(int i=0;i<n;i++)
+//         {
+//             if(arr[i]>largest )
+//             {
+//                 secondlargest=largest;
+//                 largest=arr[i];
+//             }
+//             else if(arr[i]>secondlargest && arr[i]<largest)
+//             {
+//                 secondlargest=arr[i];
+//             }
+//         }
+//         return secondlargest;
+//     }
+// };
---
+++ Arrays/linearsearch.cpp
@@ -0,0 +1,28 @@
+// classSolution{
+//     public:
+//     // Function to find element in sorted array
+//     // arr: input array
+//     // N: size of array
+//     // K: element to be searche
+//     int searchInSorted(int arr[], int N, int K) 
+//     { 
+//         int i=0;
+//         while(i<=N-1)
+//         {
+//             if(arr[i]==K)
+//             {
+//                 return 1;
+//                 break;
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+//         if(i==N)
+//         {
+//             return -1;
+//         }
+       
+       
+//     }
+// };
