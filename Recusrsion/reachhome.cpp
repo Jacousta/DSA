000cb987038dbb4d7905cafb437d73b7ffdf38d2
Jacousta <asharma22_be21@thapar.edu>
2024-07-14 16:09:35 +0530

    chrd2started

---
+++ Recusrsion/reachhome.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+void reacHome(int src,int dest)
+{
+    //base case
+    if(src==dest)
+{
+    cout<<"reached home"<<endl;
+}
+//recursive case
+
+else{
+    cout<<"going to "<<src+1<<endl;
+    reacHome(src+1,dest);
+}
+}
+int main()
+{
+    int src=1;
+    int dest=10;
+    reacHome(src,dest);
+    
+}
