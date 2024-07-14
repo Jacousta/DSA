341c80c8695d3f8c95d5346b6990a3a74dc8e686
Jacousta <asharma22_be21@thapar.edu>
2024-07-14 16:27:53 +0530

    countstairs

---
+++ Recusrsion/countstairs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int count(int n)
+{
+    //base case
+    if(n<0)
+    {
+        return 0;
+    }
+    else if(n==0)
+    {
+        return 1;
+    }
+    // recursive case
+    return count(n-1)+count(n-2);
+
+}
+int main()
+{
+    int n=5;
+    cout<<count(n);
+    
+}
