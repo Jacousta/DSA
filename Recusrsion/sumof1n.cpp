c54174514330c7198b3b78190d0d67872d0c4d63
Jacousta <asharma22_be21@thapar.edu>
2024-07-14 16:01:53 +0530

    striverrecstarted

---
+++ Recusrsion/sumof1n.cpp
@@ -0,0 +1,20 @@
+
+
+// class Solution {
+//   public:
+//   long long k=0;
+//     long long sumOfSeries(long long n) {
+//         // code here
+//         //base case
+//         if(n==1)
+//         {
+//             k=k+1;
+//         }
+//         else
+//         {
+//             k=(n*n*n)+k;
+//             sumOfSeries(n-1);
+//         }
+//         return k;
+//     }
+// };
