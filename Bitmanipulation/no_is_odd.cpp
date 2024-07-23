0bdc095f5642582f67f1036a2c075d465571ae4c
Jacousta <asharma22_be21@thapar.edu>
2024-07-23 12:02:30 +0530

    bitmanipulationstarted

---
+++ Bitmanipulation/no_is_odd.cpp
@@ -0,0 +1,13 @@
+
+        // // code here 
+        // // if(N%2==0)
+        // // {
+        // //     return "even";
+        // // }
+        // // return "odd";
+        // //with bit manipultaion
+        // if(N&(1<<0)!=0)
+        // {
+        //     return "odd";
+        // }
+        // return "even";
